6b9ed443e45083bbb12abe3254fd3f33999ad6c1
Chirag <chiragsaini6405@gmail.com>
2023-02-11 14:03:46 +0530

    update

--- src/Client/components/Nav.jsx
+++ src/Client/components/Nav.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+const iconPath = process.env.PUBLIC_URL;
+
 function Nav() {
   return (
-    <div className="NavBar p-2 bg-gray-50 border-2 fixed w-full flex flex-col md:flex-row z-40">
-      <NavLink to="/home" className="Nav_Logo m-3 ">
-        Door Step
+    <div className="NavBar p-2 bg-gradient-to-r from-gray-500 to-gray-50 border-2 fixed w-full flex flex-col md:flex-row z-40 text-[27px] m-auto">
+      <NavLink to="/home" className="Nav_Logo m-3 flex flex-row  ">
+        <img src={iconPath+"logo.jpeg"} alt="E SPorts" className="w-20  rounded-full"/>
+        <h1 className="m-auto">GamersHub</h1>
       </NavLink>
       <div className="flex md:ml-auto ml-0 md:mr-auto mr-0 w-5/6 align-middle">
         <div className="flex flex-col md:flex-row flex-wrap md:ml-auto ml-0 align-middle">
-        <NavLink to="/Home" className="Login m-3 mx-6 hover:text-red-500">
+        <NavLink to="/Home" className="Login m-auto mx-6 hover:text-red-500">
             Home
           </NavLink>
-          <NavLink to="/News" className="Login m-3 mx-6 hover:text-red-500">
+          <NavLink to="/News" className="Login m-auto mx-6 hover:text-red-500">
             News
           </NavLink>
-          <NavLink to="/Videos" className="Login m-3 mx-6 hover:text-red-500">
+          <NavLink to="/Videos" className="Login m-auto mx-6 hover:text-red-500">
             Videos
           </NavLink>
-          <NavLink to="/Deals" className="Login m-3 mx-6 hover:text-red-500">
+          <NavLink to="/Deals" className="Login m-auto mx-6 hover:text-red-500">
             Deals
           </NavLink>
-          <NavLink to="/Shop" className="Cart m-3 mx-6 hover:text-red-500">
+          <NavLink to="/Shop" className="Cart m-auto mx-6 hover:text-red-500">
             Shop
           </NavLink>
-          <select className="mx-6" name="" id="">
+          <select className="mx-6 bg-gray-50" name="" id="">
             <option value="">EN</option>
             <option value="">HN</option>
             <option value="">DE</option>
--- src/Client/components/News.jsx
+++ src/Client/components/News.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
+import "./news.css";
+
+const iconPath = process.env.PUBLIC_URL;
 
 function News() {
+
+    // var tablinks = document.getElementsByClassName("tab-links");
+    // var tabcontents = document.getElementsByClassName("tab-contents");
+    
+    // function opentab(event,tabname){
+    //     for(tablink of tablinks){
+    //         tablink.classList.remove("active-link");
+    //     }
+    //     for(tabcontent of tabcontents){
+    //         tabcontent.classList.remove("active-tab");
+    //     }
+    //     event.currentTarget.classList.add("active-link");
+
+    //     document.getElementById(tabname).classList.add("active-tab");
+    // }
   return (
-    <div>News</div>
+    <div>
+        <div id="about">
+    <div class="container">
+        <div class="row">
+            <div class="about-col-1">
+                <img src={iconPath+"newslo.jpeg" }alt="group photo" />
+            </div>
+            <div class="about-col-2">
+                <h1>NEWS</h1>
+                <p>The gaming industry is a constantly evolving and rapidly growing field, with new advancements, releases, and trends emerging all the time. From the latest video game releases and hardware updates to esports tournaments and industry events, there is always something new and exciting happening in the world of gaming.</p>
+
+
+                <div class="tab-titles">
+                    {/* <p class="tab-links active-link" onclick={opentab('COD')}>COD</p>
+                    <p class="tab-links" onclick={opentab('GTA')}>GTA</p>
+                    <p class="tab-links"onclick={opentab('SKYRIM')}>SKYRIM</p> */}
+                </div>
+
+                <div class="tab-contents active-tab" id="COD">
+                    <ul>
+                        <li>Activision confirms that Gun Game is coming to Call of Duty: Modern Warfare 2 as a part of the upcoming Season 2 update.
+                        </li>
+                        <li><a> COD: MW2's revamped Gunsmith introduces Weapon Platforms, Receivers, weapon evolutions, shared attachments, and a progression overhaul that cuts down on repetitive per-weapon/attachment unlocks. Is this what we've been waiting for?</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="tab-contents" id="GTA">
+                    <ul>
+                        <li>GTA Online Player Escapes Pursuit, But Has Bad Luck Soon After</li>
+                        <li>A GTA Online player successfully escapes from a pursuit, but then something happens that is sad in the funniest way possible.</li>
+                        
+                    </ul>
+                </div>
+
+                <div class="tab-contents" id="SKYRIM">
+                    <ul>
+                        <li>Skyrim Clip Shows Why Players Need to Be Careful With Mods</li>
+                        <li>Mods have likely played a big role in the success of Skyrim, but one clip shows why fans may want to be careful when using them.</li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
   )
 }
 
-export default News
+export default News;
